f7cf50935e9bedcda7bb49e7e756f861a2e2ecba
WStools <WStools@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 08:51:14 +0000

    Upload
    git-svn-id: https://plugins.svn.wordpress.org/wstoolsnl-klantbeoordelingen/trunk@854617 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Plugin Name ===
+Plugin Name: WStools.nl - Klantbeoordelingen
+Plugin URI: http://wstools.nl/
+Description: Geef bezoekers de mogelijkheid om uw bedrijf/webshop te beoordelen
+Version: 1.0
+Author: WStools.nl
+Author URI: http://wstools.nl/
+
+Contributors: WStools
+Donate link: http://wstools.nl/
+Tags: website, klantbeoordelingen, klanten reviews, klanten feedback, 
+Requires at least: 3.0.1
+Tested up to: 3.8.1
+Stable tag: 4.3
+
+Geef bezoekers de mogelijkheid om uw bedrijf/webshop te beoordelen met de klantbeoordeling plugin van WStools.nl
+
+== Description ==
+
+Met de klantbeoordeling plugin voor WordPress kunt u de klantbeoordelingen tool van WStools.nl gemakkelijk integreren in uw WordPress website.
+Voor deze plugin is een geregistreerde account nodig van [WStools.nl](http://wstools.nl/).
+
+= Zoekmachine geoptimaliseerd =
+Wanneer u genoeg ervaringen heeft ontvangen worden de klantbeoordelingen ook in de zoekmachine getoond met sterren. Zowel bij uw home pagina als in uw advertenties. De kans dat mensen klikken op uw advertentie wordt hierdoor vergroot.
+
+= Zelf vragen instellen =
+Naast een algemene beoordeling kunt u de bezoeker zelf een beoordeling vragen. U kunt hierdoor bijvoorbeeld vragen wat bezoeker vond van de service en/of de prijs.
+
+= Beheer zelf uw klantbeoordelingen =
+U kunt zelf in ons systeem uw beoordelingen beheren. Geef bijvoorbeeld als website eigenaar een reactie op een beoordeling.
+
+== Installation ==
+Na het activeren van de plugin kan er een widget geplaatst worden in uw sidebar. Om de klantbeoordelingen te tonen in uw teksten kunt u [WStools-klantbeoordelingen] gebruiken als shortcode.
+
+== Frequently Asked Questions ==
+
+= What about foo bar? =
+
+== Screenshots ==
+
+1. Website weergaven klantbeoordeling
+2. Beheer klantbeoordeling
+
+== Changelog ==
+
+== Upgrade Notice ==
+
+== Arbitrary section ==
+
+== A brief Markdown Example ==
+
+<?php code(); // goes in backticks ?>`
---
+++ wstools-klantbeoordelingen.php
@@ -0,0 +1,95 @@
+<?php
+/**
+Plugin Name: WStools.nl Klantbeoordelingen
+Plugin URI: http://wstools.nl/
+Description: Klantbeoordelingen module van WStools.nl
+Version: 1.0
+Author: WStools.nl
+Author URI: http://wstools.nl/
+*/
+class WStools_klantbeoordelingen extends WP_Widget
+{
+  function WStools_klantbeoordelingen()
+  {
+    $widget_ops = array('classname' => 'WStools_klantbeoordelingen', 'description' => 'Toon uw klantbeoordelingen' );
+    $this->WP_Widget('WStools_klantbeoordelingen', 'Klantbeoordelingen', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Soort: 
+  	<select name="<?php echo $this->get_field_name('title'); ?>">
+		<option value="Box 1"<?php if(attribute_escape($title) == "Box 1") { ?> selected<?php } ?>>Box 1</option>
+  		<option value="Box 2"<?php if(attribute_escape($title) == "Box 2") { ?> selected<?php } ?>>Box 2</option>
+  	</select>
+  </label>
+  </p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 	if(attribute_escape($title) == "") { $soort = "Box 1"; } else { $soort = attribute_escape($title);  }
+
+	$host = $_SERVER['HTTP_HOST'];
+
+	if($soort == "Box 1") {
+		$linkbox1 = file_get_contents("http://beoordelingen.wstools.nl/give/".$host."/klantbeoordelingen-box01-link");
+		$bron = file_get_contents($linkbox1."&wp=TRUE&check=".urlencode(str_replace("/","|",$_SERVER['REQUEST_URI'])));
+		$bron = str_replace("{wppluginroot}",plugin_dir_url().'WStools-klantbeoordelingen/',$bron);
+		if ($bron != "") {
+		wp_enqueue_script('WSKlant', 'http://beoordelingen.wstools.nl/js/WS-klantbeoordelingen.js');
+		wp_enqueue_style('WSKlantcss', plugin_dir_url().'WStools-klantbeoordelingen/css/WStools-box1.css');
+		?>
+        <div class="box-ws-in"><?php echo $bron; ?></div>
+        <?php
+		}
+	} 
+	if($soort == "Box 2") {
+		$linkbox2 = file_get_contents("http://beoordelingen.wstools.nl/give/".$host."/klantbeoordelingen-box02-link");
+		$linkbox2a = file_get_contents("http://beoordelingen.wstools.nl/give/".$host."/klantbeoordelingen-box02-linka");
+		if ($linkbox2 != "") {
+			wp_enqueue_style('WSKlant', plugin_dir_url().'WStools-klantbeoordelingen/css/WStools-box2.css');
+		?>
+        <div class="box2-wstools">
+        	<div class="box2-top">klantbeoordelingen</div>
+    		<iframe src="<?php echo $linkbox2; ?>" scrolling="no" width="100%" frameborder="0" /></iframe>
+            <div class="box2-bottom">
+                <div class="WStools-inc"><a href="<?php echo $linkbox2a; ?>" target="_blank" title="Gerealiseerd door WStools.nl"><img src="<?php echo plugin_dir_url().'WStools-klantbeoordelingen/'; ?>images/wstools-fullwit.png" alt="WStools" /></a></div>
+            </div>
+        </div>                
+        <?php
+		}
+	}
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("WStools_klantbeoordelingen");') );
+
+
+
+function WStools_klantbeoordeling_iframe(){
+	$host = $_SERVER['HTTP_HOST'];
+	$iframecode = file_get_contents("http://beoordelingen.wstools.nl/give/".$host."/klantbeoordelingen-iframe");
+	wp_enqueue_script('WSFrame', plugin_dir_url().'WStools-klantbeoordelingen/js/WS-Frame.js');
+	?>
+	<iframe src="<?php echo $iframecode; ?>" id="WStools-kframe" scrolling="no" width="100%" height="200px" frameborder="0" onload="FrameManager.registerFrame(this)" /></iframe>
+    <?php
+}
+
+
+add_shortcode( 'WStools-klantbeoordelingen', 'WStools_klantbeoordeling_iframe' );
+
+?>
